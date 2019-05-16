65701506021e59249eed4de7f7c74b9ad85fe04f
Igor Spasić <igor@jodd.org>
2019-05-16 22:23:41 +0200

    Updated to Spig v1.5.3

--- src/filters/datetimefmt.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const {DateTime} = require("luxon");
-
-module.exports = {
-  dateDisplay: (dateObj, format = "LLL d, y") => {
-    return DateTime.fromJSDate(dateObj, {zone: "utc"}).toFormat(format);
-  }
-};
